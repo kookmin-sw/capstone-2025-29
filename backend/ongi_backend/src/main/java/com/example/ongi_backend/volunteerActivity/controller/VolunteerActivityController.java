c43ba32eb4db6c1e95b6e4ad933652d40a9842ed
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2025-03-17 22:54:35 +0900

    Feat : 완료된 작업 조회 - #10
    - Complete Activities Response Dto 생성
    - 기본 Controller 세팅
    - Asia/Seoul timeZone 명시

---
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/volunteerActivity/controller/VolunteerActivityController.java
@@ -0,0 +1,53 @@
+package com.example.ongi_backend.volunteerActivity.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ongi_backend.volunteerActivity.dto.ResponseVolunteerActivities;
+import com.example.ongi_backend.volunteerActivity.service.VolunteerActivityService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/volunteerActivity")
+public class VolunteerActivityController {
+	private final VolunteerActivityService volunteerActivityService;
+
+	//TODO : 추후 Redis를 통해 조회
+	@GetMapping
+	public ResponseEntity<?> getVolunteerActivityList() {
+		return null;
+	}
+
+	@GetMapping("/complete")
+	public ResponseEntity<List<ResponseVolunteerActivities>> getCompleteVolunteerActivities(Principal principal) {
+		return ResponseEntity.ok(volunteerActivityService.findCompleteVolunteerActivities(
+			"username"
+			//TODO : 로그인 구현 후 코드 수정
+			// principal.getName()
+		));
+	}
+
+	@GetMapping("/incomplete")
+	public ResponseEntity<?> getIncompleteVolunteerActivityList() {
+		return null;
+	}
+
+	@GetMapping("/registered")
+	public ResponseEntity<?> getRegisteredVolunteerActivityList() {
+		return null;
+	}
+
+	@GetMapping("{volunteerActivityId}")
+	public ResponseEntity<?> getVolunteerActivityDetail(@PathVariable String volunteerActivityId) {
+		return null;
+	}
+
+}
