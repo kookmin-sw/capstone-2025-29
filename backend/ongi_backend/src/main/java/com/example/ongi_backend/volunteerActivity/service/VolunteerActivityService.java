941396455a4568296ab036f12a1abae946f3e26b
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2025-03-18 10:59:59 +0900

    Feat : 특정 id 작업 조회 - #10
    - Address District enum 으로 변경
    - 기존 봉사 활동 entity에서 애완동물 여부 확인에서 애완동물 enum 타입으로 변경
    - Response Dto 추가

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/volunteerActivity/service/VolunteerActivityService.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/volunteerActivity/service/VolunteerActivityService.java
@@ -1,11 +1,16 @@
 package com.example.ongi_backend.volunteerActivity.service;
 
+import static com.example.ongi_backend.global.exception.ErrorCode.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.ongi_backend.global.entity.Address;
+import com.example.ongi_backend.global.exception.CustomException;
+import com.example.ongi_backend.volunteerActivity.dto.ResponseActivityDetail;
 import com.example.ongi_backend.volunteerActivity.dto.ResponseVolunteerActivities;
 import com.example.ongi_backend.volunteerActivity.entity.VolunteerActivity;
 import com.example.ongi_backend.volunteerActivity.repository.VolunteerActivityRepository;
@@ -29,7 +34,7 @@ public class VolunteerActivityService {
 		}).collect(Collectors.toList());
 	}
 
-	public List<?> findRegisteredActivities(String username) {
+	public List<ResponseVolunteerActivities> findRegisteredActivities(String username) {
 		List<VolunteerActivity> completeList = volunteerActivityRepository.findRegisteredActivitiesByUserName(username);
 		return completeList.stream().map(va -> {
 			return ResponseVolunteerActivities.builder()
@@ -38,5 +43,25 @@ public class VolunteerActivityService {
 				.time(va.getStartTime())
 				.build();
 		}).collect(Collectors.toList());
+	}
+
+	public ResponseActivityDetail findActivityDetail(Long volunteerActivityId) {
+		return volunteerActivityRepository.findById(volunteerActivityId)
+			.map(va -> {
+				return ResponseActivityDetail.builder()
+					.id(va.getId())
+					.type(va.getType())
+					.startTime(va.getStartTime())
+					.animalType(va.getAnimalType())
+					.addDescription(va.getAddDescription())
+					.address(
+						Address.builder()
+							.detail(va.getAddress().getDetail())
+							.district(va.getAddress().getDistrict())
+							.build()
+					)
+					.build();
+			})
+			.orElseThrow(() -> new CustomException(NOT_FOUND_VOLUNTEER_ACTIVITY_ERROR));
 	}
 }
