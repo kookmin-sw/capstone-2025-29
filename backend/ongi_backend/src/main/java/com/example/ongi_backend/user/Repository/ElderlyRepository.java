f0e00d64690fb1708db0485a909bf7023a78ad7b
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2025-03-18 23:40:22 +0900

    Feat: 로그인 시 회원정보 조회를 위한 메서드 추가 - #12 - 봉사자, 독거노인 Repository에 Username 기반 조회 메서드 추가

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/user/Repository/ElderlyRepository.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/user/Repository/ElderlyRepository.java
@@ -4,7 +4,10 @@ import com.example.ongi_backend.user.entity.Elderly;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ElderlyRepository extends JpaRepository<Elderly, Long> {
     boolean existsByUsername(String username);
+    Optional<Elderly> findByUsername(String username);
 }
