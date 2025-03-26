d3b5163b08ef3e4175241d25ef9500a1c074d035
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2025-03-26 22:35:36 +0900

    Refactor : DB 수정 - #10
    - image 테이블 분리
    - 봉사 상태 enum으로 수정(완료, 진행중, 후기작성)
    - 봉사 모집 상태 속성 삭제

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/review/entity/BaseReview.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/review/entity/BaseReview.java
@@ -2,18 +2,23 @@ package com.example.ongi_backend.review.entity;
 
 import static jakarta.persistence.GenerationType.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.ongi_backend.global.entity.BaseEntity;
+import com.example.ongi_backend.image.entity.Image;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToMany;
 
 @MappedSuperclass
 public abstract class BaseReview extends BaseEntity {
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	private Long id;
-	//TODO: 사진을 여러개 올릴수 있나?
-	private String photo;
+	@OneToMany(mappedBy = "baseReview")
+	private List<Image> images = new ArrayList<>();
 	private String content;
 }
--- backend/ongi_backend/src/main/java/com/example/ongi_backend/volunteerActivity/repository/VolunteerActivityRepository.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/volunteerActivity/repository/VolunteerActivityRepository.java
@@ -11,7 +11,7 @@ import com.example.ongi_backend.volunteerActivity.entity.VolunteerActivity;
 
 @Repository
 public interface VolunteerActivityRepository extends JpaRepository<VolunteerActivity, Long> {
-	@Query("SELECT va FROM VolunteerActivity va JOIN FETCH va.elderly JOIN FETCH va.volunteer WHERE va.volunteer.username = :username AND va.isDone = true")
+	@Query("SELECT va FROM VolunteerActivity va JOIN FETCH va.elderly JOIN FETCH va.volunteer WHERE va.volunteer.username = :username AND va.status = 'COMPLETED'")
 	List<VolunteerActivity> findCompleteActivitiesByUserName(String username);
 	@Query("SELECT va FROM VolunteerActivity va JOIN FETCH va.elderly WHERE va.elderly.username = :username")
 	List<VolunteerActivity> findRegisteredActivitiesByUserName(String username);
