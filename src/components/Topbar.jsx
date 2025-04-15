5134f444ee4cfc2410d96c1ac8f61c508efb5a6e
glory-yun <yyk1797@gmail.com>
2025-04-15 17:47:15 +0900

    edit name

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./TopBar.module.css";
+import styles from "./Topbar.module.css";
 
 export default function Topbar({ title, navigateTo }) {
     const navigate = useNavigate();
