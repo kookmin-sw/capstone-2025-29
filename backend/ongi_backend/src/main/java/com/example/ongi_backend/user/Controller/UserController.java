322145c968602851906c4c55c473020b88a9932d
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2025-04-16 12:59:02 +0900

    Docs : 유저 정보 API Swagger 작성 - #75

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/user/Controller/UserController.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/user/Controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.ongi_backend.user.Dto.RequestModifyPassword;
 import com.example.ongi_backend.user.Dto.ResponseUserInfo;
 import com.example.ongi_backend.user.Dto.UserRegisterDto;
 import com.example.ongi_backend.user.Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ public class UserController {
     private final NotificationRedisService notificationRedisService;
 
     @GetMapping
+    @Operation(summary = "유저 정보 가져오기", description = "수정 페이지 들어갈때 유저 정보 불러오기")
     public ResponseEntity<ResponseUserInfo> getUser(Principal principal, @RequestParam String userType) {
         ResponseUserInfo responseUserInfo = userService.getUser(principal.getName(), userType);
         return ResponseEntity.ok().body(responseUserInfo);
@@ -36,30 +38,35 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "회원가입", description = "유저 저장")
     public ResponseEntity<?> addUser(@RequestBody UserRegisterDto userRegisterDto) {
         userService.saveUser(userRegisterDto);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
+    @Operation(summary = "유저 수정", description = "마이페이지 눌렀을때 유저 수정")
     public ResponseEntity<?> modifyUser(@RequestBody RequestModify requestModify, Principal principal) {
         userService.modifyUser(requestModify, principal.getName());
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/username")
+    @Operation(summary = "아이디 중복 체크", description = "회원가입 시 아이디 중복 체크")
     public ResponseEntity<?> duplicateCheck(@RequestParam String username, @RequestParam String userType) {
         userService.duplicateCheck(username, userType);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/password")
+    @Operation(summary = "비밀번호 체크", description = "수정 페이지 비밀번호 확인")
     public ResponseEntity<?> passwordCheck(Principal principal, @RequestParam String password, @RequestParam String userType) {
         userService.passwordCheck(principal.getName(), userType, password);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping
+    @Operation(summary = "비밀번호 수정", description = "수정 페이지 비밀번호 수정")
     public ResponseEntity<?> modifyPassword(Principal principal, @RequestBody RequestModifyPassword requestModifyPassword) {
         userService.modifyPassword(principal.getName(), requestModifyPassword);
         return ResponseEntity.ok().build();
