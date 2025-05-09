9c73c58c5c04eb4a6dd9989194ba3c3cf84ae3a7
Sung-young <smes289@gmail.com>
2025-05-09 13:00:08 +0900

    docker빌드 테스트

--- AI/main.py
+++ AI/main.py
@@ -16,7 +16,7 @@ from modules.auth import get_current_user
 from modules.delete_audiofile import delete_file_after_delay
 
 
-
+#test
 app = FastAPI()
 app.mount("/audio", StaticFiles(directory="tts_output"), name="audio")
 
