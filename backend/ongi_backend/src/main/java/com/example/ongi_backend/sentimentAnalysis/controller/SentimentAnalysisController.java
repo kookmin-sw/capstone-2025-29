1ef93d47916a78459694a3f738f0ad16a99f9224
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2025-05-10 22:56:58 +0900

    Feat: 감정분석 결과 조회 API 구현 - #94 - 감정분석 Controller, Service, Repository 수정 - 감정결과에 ChatBot이름도 함께 반환

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/sentimentAnalysis/controller/SentimentAnalysisController.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/sentimentAnalysis/controller/SentimentAnalysisController.java
@@ -1,10 +1,14 @@
 package com.example.ongi_backend.sentimentAnalysis.controller;
 
 import com.example.ongi_backend.sentimentAnalysis.dto.RequestSentimentAnalysis;
+import com.example.ongi_backend.sentimentAnalysis.dto.ResponseSentimentAnalysis;
 import com.example.ongi_backend.sentimentAnalysis.service.SentimentAnalysisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +20,11 @@ public class SentimentAnalysisController {
     public ResponseEntity<?> saveEmotion(@RequestBody RequestSentimentAnalysis request) {
         sentimentAnalysisService.saveSentimentAnalysis(request);
         return ResponseEntity.ok("감정 분석 저장");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getSentimentAnalysis(Principal principal) {
+        ResponseSentimentAnalysis response = sentimentAnalysisService.getSentimentAnalysis(principal.getName());
+        return ResponseEntity.ok(response);
     }
 }
