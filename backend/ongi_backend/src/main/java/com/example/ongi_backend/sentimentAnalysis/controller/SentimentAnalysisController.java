f47a251bc3db753cc70fca966ace1804abd976dc
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2025-05-10 23:07:15 +0900

    Docs: Swagger 작성 - #94 - 감정분석, Elderly Controller Swagger 작성

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/sentimentAnalysis/controller/SentimentAnalysisController.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/sentimentAnalysis/controller/SentimentAnalysisController.java
@@ -3,6 +3,7 @@ package com.example.ongi_backend.sentimentAnalysis.controller;
 import com.example.ongi_backend.sentimentAnalysis.dto.RequestSentimentAnalysis;
 import com.example.ongi_backend.sentimentAnalysis.dto.ResponseSentimentAnalysis;
 import com.example.ongi_backend.sentimentAnalysis.service.SentimentAnalysisService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +18,15 @@ public class SentimentAnalysisController {
     private final SentimentAnalysisService sentimentAnalysisService;
 
     @PostMapping
+    @Operation(summary = "감정 분석 결과 저장", description = "감정 분석 결과 저장")
     public ResponseEntity<?> saveEmotion(@RequestBody RequestSentimentAnalysis request) {
         sentimentAnalysisService.saveSentimentAnalysis(request);
         return ResponseEntity.ok("감정 분석 저장");
     }
 
     @GetMapping
-    public ResponseEntity<?> getSentimentAnalysis(Principal principal) {
+    @Operation(summary = "감정 분석 결과 불러오기", description = "감정 분석 결과 불러오기")
+    public ResponseEntity<ResponseSentimentAnalysis> getSentimentAnalysis(Principal principal) {
         ResponseSentimentAnalysis response = sentimentAnalysisService.getSentimentAnalysis(principal.getName());
         return ResponseEntity.ok(response);
     }
