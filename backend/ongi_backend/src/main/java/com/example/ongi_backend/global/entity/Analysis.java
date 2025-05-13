419096cbfd9a6e1eb914cd780caae63586a79977
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2025-05-13 21:41:39 +0900

    Refactor: Analysis Enum에서 설렘 제거, 슬픔(SAD) 추가

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/global/entity/Analysis.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/global/entity/Analysis.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Analysis {
-	JOY("기쁨"), EXCITEMENT("신남"), ANTICIPATION("설렘"),
+	JOY("기쁨"), EXCITEMENT("신남"), SAD("슬픔"),
 	ANGER("분노"), CALM("평온"), LONELINESS("외로움"), FEAR("두려움");
 
 	Analysis(String description) {
