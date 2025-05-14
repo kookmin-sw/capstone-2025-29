e029128ec9ef1dae113221a4f73b995069780c0b
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2025-05-14 20:42:31 +0900

    Feat : redis 만료 이벤트 설정 - #92

--- backend/ongi_backend/src/main/java/com/example/ongi_backend/global/config/RedisConfig.java
+++ backend/ongi_backend/src/main/java/com/example/ongi_backend/global/config/RedisConfig.java
@@ -8,14 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@EnableRedisRepositories(shadowCopy = OFF, enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.OFF, keyspaceNotificationsConfigParameter = "")
+@EnableRedisRepositories(shadowCopy = OFF, enableKeyspaceEvents = ON_DEMAND, keyspaceNotificationsConfigParameter = "")
 public class RedisConfig {
 	@Value("${spring.data.redis.host}")
 	private String host;
