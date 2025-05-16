e009a26fae56599fab7d63307351e31126bbdffc
glory-yun <yyk1797@gmail.com>
2025-05-16 16:15:11 +0900

    add code

--- src/api/RedirectHandler.jsx
+++ src/api/RedirectHandler.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { updateFcmToken  } from '../api/both'; // ✅ FCM, Elderly 정보 API
-import { fetchElderlyMatching } from '../api/UserApi'; // ✅ Elderly 정보 API
+import { fetchElderlyMatching,fetchUserInfo } from '../api/UserApi'; // ✅ Elderly 정보 API
 export default function RedirectHandler() {
     const navigate = useNavigate();
     const location = useLocation();
@@ -14,11 +14,13 @@ export default function RedirectHandler() {
         const refreshToken = params.get('refreshToken');
         const userType = params.get('userType');
 
+        console.log('accessToken', accessToken, 'refreshToken', refreshToken, 'userType', userType)
         // ✅ localStorage 저장 (토큰 및 userType)
         if (accessToken) localStorage.setItem('accessToken', accessToken);
         if (refreshToken) localStorage.setItem('refreshToken', refreshToken);
         if (userType) localStorage.setItem('userType', userType)
 
+        
         const init = async () => {
             try {
                 // ✅ FCM 토큰 등록
@@ -27,9 +29,10 @@ export default function RedirectHandler() {
 
                 // ✅ elderly인 경우: 나의 정보 조회
                 if (userType === 'elderly') {
-                    const elderlyInfo = await fetchElderlyMatching(accessToken);
+                    const elderlyInfo = await fetchUserInfo(accessToken);
 
                     console.log('elderlyInfo:', elderlyInfo);
+
 
                     // ✅ 이름, 주소 로컬스토리지 저장
                     if (elderlyInfo.name) localStorage.setItem('userName', elderlyInfo.name);
@@ -52,5 +55,5 @@ export default function RedirectHandler() {
 
     }, [location.search, navigate]);
 
-    return <div>로그인 중입니다...</div>;
+    return null; // 렌더링할 내용이 없으므로 null 반환
 }
