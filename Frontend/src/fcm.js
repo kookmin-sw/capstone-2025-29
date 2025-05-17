d910f5818cb7a4b556374047fd98c456337ede92
glory-yun <yyk1797@gmail.com>
2025-05-18 01:28:26 +0900

    update volinteermain.jsx

--- Frontend/src/fcm.js
+++ Frontend/src/fcm.js
@@ -2,7 +2,6 @@
 import { messaging, getToken } from './firebase';
 
 export const requestFCMToken = async () => {
-    alert(`fcmtoken!!!!!!!!! ', ${fcmToken}`)
     try {
         const currentToken = await getToken(messaging, {
             vapidKey: "BPmZvGshmU9engJbGw4Ny05svrAhclGNLlgUXfsjw9_EJp0jX80M0wzra6bcN9fXD0WVG2WMale74xxbQGQL3tw"
@@ -10,7 +9,7 @@ export const requestFCMToken = async () => {
 
         
         localStorage.setItem('fcmToken', currentToken); // 토큰을 로컬 스토리지에 저장
-
+        alert(`fcmtoken!!!!!!!!! ', ${currentToken}`)
     } catch (err) {
         console.error('An error occurred while retrieving token. ', err);
     }
