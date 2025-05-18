9eb8166056ede6b2208077715445c0a1d4669cc4
glory-yun <yyk1797@gmail.com>
2025-05-19 07:06:23 +0900

    update alarm logic

--- Frontend/src/pages/user/UserMain.jsx
+++ Frontend/src/pages/user/UserMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import styles from "./UserMain.module.css";
 import ongi from "../../assets/ongi.svg";
 import { useNavigate } from "react-router-dom";
